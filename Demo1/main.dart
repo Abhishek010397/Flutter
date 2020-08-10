f521a7636cc6e6edb66a2af804642bacca792491
ABHISHEK DASGUPTA <abhishekdg7880@gmail.com>
2020-08-10 22:07:58 +0530

    Flutter Word Generator Application

--- Demo1/main.dart
+++ Demo1/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,7 +7,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
+    final wordPair =WordPair.random();
     return MaterialApp (
       title: 'Welcome to Flutter Environment',
       home: Scaffold (
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
           title: Text('Abhishek'),
         ),
         body: Center(
-          child: Text('Hello')
+          child: Text(wordPair.asPascalCase)
         ),
       ),
     );
